3cd94c1c7fb0de99b1342f80fa6a4d349b3841d0
Ruan Swanepoel <ruane.swanepoel@gmail.com>
2026-05-19 16:05:37 +1000

    temp

---
+++ src/notion_parser.ts
@@ -0,0 +1,49 @@
+import { Client } from "@notionhq/client";
+import { NotionToMarkdown } from "notion-to-md";
+import type { BlockChildrenResponseExtended, NotionPage } from "./types.js";
+import type { ListBlockChildrenResponseResults } from "notion-to-md/build/types/index.js";
+
+export class NotionParser {
+  notionClient: Client;
+  n2m: NotionToMarkdown;
+
+  constructor(notion_secret: string) {
+    this.notionClient = new Client({
+      auth: notion_secret,
+    });
+
+    this.n2m = new NotionToMarkdown({
+      notionClient: this.notionClient,
+      config: {
+        parseChildPages: false,
+      },
+    });
+  }
+
+  /**
+   * Retrieves a single Notion page and conveniently converts the content to markdown and finds the child pages.
+   */
+  async retrievePage(pageId: string): NotionPage {
+    const blocks = (await this.notionClient.blocks.children.list({
+      block_id: pageId,
+    })) as unknown as { results: BlockChildrenResponseExtended[] };
+
+    const childPages = blocks.results.filter(
+      (page) => page.type == "child_page",
+    );
+
+    // Convert to markdown
+    const mdBlocks = await this.n2m.blocksToMarkdown(
+      blocks.results as ListBlockChildrenResponseResults,
+    );
+    const mdString = this.n2m.toMarkdownString(mdBlocks);
+
+    return {
+      blocks,
+      mdString,
+      childPages,
+    };
+  }
+
+  async parsePage
+}
---
+++ src/page_node.ts
@@ -0,0 +1,58 @@
+import type { Notion } from "./notion.js";
+import type { BlockChildrenResponseExtended } from "./types.js";
+
+/**
+ * Represents a notion page (content node) with its own content, metadata, and sub-pages.
+ */
+export type PageNode = {
+  notionId: string;
+  notionTitle: string;
+  notionPage:
+    | ({ metadata?: BlockChildrenResponseExtended } & Partial<
+        Awaited<ReturnType<typeof Notion.prototype.retrievePage>>
+      >)
+    | null;
+  parentNode: PageNode | null;
+  childNodes: PageNode[];
+};
+
+/**
+ * Builds the page/node tree according to the layout in Notion, starting from the given root page ID.
+ */
+export async function buildPageTree(rootId: string, notion: Notion) {
+  const rootNode: PageNode = {
+    notionId: rootId,
+    notionTitle: "Root",
+    notionPage: null,
+    parentNode: null,
+    childNodes: [],
+  };
+  let queue = [rootNode];
+
+  for (let i = 0; i < queue.length; i++) {
+    const node = queue[i]!; // Currunt node, never undefined
+    const retrievedPage = await notion.retrievePage(node.notionId);
+    node.notionPage = {
+      ...node.notionPage, // Preserve existing metadata
+      ...retrievedPage,
+    };
+
+    if (!node.notionPage?.childPages) continue;
+
+    for (let cp of node.notionPage?.childPages) {
+      const newNode = {
+        notionId: cp.id,
+        notionTitle: cp.child_page.title,
+        notionPage: {
+          metadata: cp,
+        },
+        parentNode: node,
+        childNodes: [],
+      };
+      node.childNodes.push(newNode);
+      queue.push(newNode);
+    }
+  }
+
+  return rootNode;
+}
